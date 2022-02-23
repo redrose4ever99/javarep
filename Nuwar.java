b331e6dc350e6a5d36dd09c2939e4a5a30230348
redrose4ever99 <60423374+redrose4ever99@users.noreply.github.com>
2022-02-23 12:43:08 +0200

    sec

---
+++ Nuwar.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Nuwar {
+    public static void main(String args[])
+    {
+        JOptionPane.showMessageDialog(null,null,"rewre", 0);
+        
+    }
+    
+}
