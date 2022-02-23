1a5557a42981b20dafd22b8630a2ffc4b6c1dfc4
redrose4ever99 <60423374+redrose4ever99@users.noreply.github.com>
2022-02-23 12:45:56 +0200

    hel

--- Nuwar.java
+++ Nuwar.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class Nuwar {
     public static void main(String args[])
     {
-        JOptionPane.showMessageDialog(null,null,"rewre", 0);
+        JOptionPane.showMessageDialog(null,"Hello Gofran","warn",0);
         
     }
     
